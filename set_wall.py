6c3129012f43964c5218cf4d673ea7733c788adb
Sumedh Rao <sumedhrao@infrrd.ai>
2019-06-30 20:45:19 +0530

    added Linux support

--- set_wall.py
+++ set_wall.py
@@ -1,5 +1,12 @@
 import ctypes
 import os
+import platform
 
 def apply_wallpaper(fn):
-    return ctypes.windll.user32.SystemParametersInfoW( 20 , 0 , os.path.abspath( fn ) , 0 )
+    if platform.system() == "Windows":
+        return ctypes.windll.user32.SystemParametersInfoW( 20 , 0 , os.path.abspath( fn ) , 0 )
+    if platform.system() == "Linux":
+        try:
+            return os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri "+os.path.abspath( fn ))
+        except:
+            return os.system("gconftool-2 --type=string --set /desktop/gnome/background/picture_filename "+os.path.abspath( fn ))
